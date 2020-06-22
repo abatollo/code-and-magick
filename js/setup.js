f4ae0d81b0614d321686fc7ddaa90f2401b6c8c0
Aleksandr Batollo <a@batollo.ru>
2020-06-22 15:37:43 +0300

    Исправил логику работы с объектами для отрисовки — они передаются функции как аргумент. Исправил способ заполнения массива — используется метод push. Вынес константы в начало кода.

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,44 @@
 'use strict';
 
+var NAMES_LIST = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var SURNAMES_LIST = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+var COAT_COLOR_LIST = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var EYES_COLOR_LIST = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
 var getRandomArrayItem = function (arr) {
   var randomArrayItem = arr[Math.floor(Math.random() * arr.length)];
 
@@ -8,53 +47,14 @@ var getRandomArrayItem = function (arr) {
 
 var generateRandomWizards = function (arrLength) {
 
-  var NAMES_LIST = [
-    'Иван',
-    'Хуан Себастьян',
-    'Мария',
-    'Кристоф',
-    'Виктор',
-    'Юлия',
-    'Люпита',
-    'Вашингтон'
-  ];
-
-  var SURNAMES_LIST = [
-    'да Марья',
-    'Верон',
-    'Мирабелла',
-    'Вальц',
-    'Онопко',
-    'Топольницкая',
-    'Нионго',
-    'Ирвинг'
-  ];
-
-  var COAT_COLOR_LIST = [
-    'rgb(101, 137, 164)',
-    'rgb(241, 43, 107)',
-    'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)',
-    'rgb(215, 210, 55)',
-    'rgb(0, 0, 0)'
-  ];
-
-  var EYES_COLOR_LIST = [
-    'black',
-    'red',
-    'blue',
-    'yellow',
-    'green'
-  ];
-
   var arr = [];
 
   for (var i = 0; i < arrLength; i++) {
-    arr[i] = {
+    arr.push({
       name: getRandomArrayItem(NAMES_LIST) + ' ' + getRandomArrayItem(SURNAMES_LIST),
       coatColor: getRandomArrayItem(COAT_COLOR_LIST),
       eyesColor: getRandomArrayItem(EYES_COLOR_LIST)
-    };
+    });
   }
 
   return arr;
@@ -70,12 +70,12 @@ var renderWizard = function (wizard, templateContent) {
   return wizardElement;
 };
 
-var renderWizards = function (templateId) {
+var renderWizards = function (wisardsArray, templateId) {
   var fragment = document.createDocumentFragment();
   var wizardTemplateContent = document.querySelector(templateId).content.querySelector('.setup-similar-item');
 
-  for (var i = 0; i < wisards.length; i++) {
-    fragment.appendChild(renderWizard(wisards[i], wizardTemplateContent));
+  for (var i = 0; i < wisardsArray.length; i++) {
+    fragment.appendChild(renderWizard(wisardsArray[i], wizardTemplateContent));
   }
 
   document.querySelector('.setup-similar-list').appendChild(fragment);
@@ -88,4 +88,4 @@ hiddenBlock.classList.remove('hidden');
 
 var wisards = generateRandomWizards(4);
 
-renderWizards('#similar-wizard-template');
+renderWizards(wisards, '#similar-wizard-template');
